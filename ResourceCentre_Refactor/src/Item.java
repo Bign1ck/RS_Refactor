fcfde004ad3fdb662427afdb86b6c8829b3dc8db
LionCat79 <132029041+LionCat79@users.noreply.github.com>
2023-07-17 14:02:54 +0800

    Camcorder and Chromebook

--- ResourceCentre_Refactor/src/Item.java
+++ ResourceCentre_Refactor/src/Item.java
@@ -12,6 +12,7 @@ public class Item {
 	}
 
 	public String toString() {
+<<<<<<< Updated upstream
 		String avail = showAvailability(isAvailable);
 		String itemInfo = String.format("%-10s %-30s %-10s %-10s",
 				assetTag,
@@ -21,6 +22,19 @@ public class Item {
 		return itemInfo;
 	}
 
+=======
+		
+		// Write your codes here
+		String.format("%-10s %-30s %-10s %-10s",
+				assetTag,
+				description,
+				showAvailability(isAvailable),
+				dueDate);
+		
+		return null;
+	}
+	
+>>>>>>> Stashed changes
 	public String showAvailability(boolean isAvailable) {
 		String avail;
 
@@ -31,7 +45,11 @@ public class Item {
 		}
 		return avail;
 	}
+<<<<<<< Updated upstream
 
+=======
+	
+>>>>>>> Stashed changes
 	public String getAssetTag() {
 		return assetTag;
 	}
