852862e6e6f2a1c44995b508bb470ffb2fb590ba
NicholasTee <22005813@myrp.edu.sg>
2023-07-17 18:58:28 +0800

    BeforeBroken

--- ResourceCentre_Refactor/src/Item.java
+++ ResourceCentre_Refactor/src/Item.java
@@ -11,11 +11,28 @@ public class Item {
 		this.dueDate = "";
 		this.isAvailable = true;
 	}
+
 	public String toString() {
-		
-		// Write your codes here
-		return null;
+
+		String avail = showAvailability(isAvailable);
+		String itemInfo = String.format("%-10s %-30s %-10s %-10s",
+				assetTag,
+				description,
+				avail,
+				dueDate);
+		return itemInfo;
 	}
+
+	public String showAvailability(boolean isAvailable) {
+		String avail;
+		if (isAvailable == true) {
+			avail = "Yes";
+		} else {
+			avail = "No";
+		}
+		return avail;
+	}
+
 	public String getAssetTag() {
 		return assetTag;
 	}
@@ -40,4 +57,3 @@ public class Item {
 		this.isAvailable = isAvailable;
 	}
 }
-
